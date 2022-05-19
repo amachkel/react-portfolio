68a9b1432dc5eb2400de7ef77d6fb21daa26b09b
amachkel <amachkel@gmail.com>
2022-05-19 14:53:57 -0400

    add carousel slider for projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,72 @@
-import React from 'react'
-import '../../assets/styles'
+import React, { useState } from 'react'
+import '../../assets/styles/index.scss'
+import Slider from 'react-slick'
+import candyCrush from '../../assets/thumbnails/candy-crush.png'
+import gaHikes from '../../assets/thumbnails/ga-hikes.png'
+import hardbackLife from '../../assets/thumbnails/hardback-life.png'
+import codeQuiz from '../../assets/thumbnails/code-quiz.png'
+import teamProfile from '../../assets/thumbnails/team-profile-generator.png'
+import techBlog from '../../assets/thumbnails/tech-blog.png'
+import weatherDashboard from '../../assets/thumbnails/weatherDashboard.png'
+import rockPaperScissors from '../../assets/thumbnails/rock-paper-scissors.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from '@fortawesome/free-solid-svg-icons'
+
+const images = [
+  candyCrush,
+  gaHikes,
+  hardbackLife,
+  codeQuiz,
+  teamProfile,
+  techBlog,
+  weatherDashboard,
+  rockPaperScissors,
+]
 
 export default function Projects() {
-  return <div></div>
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FontAwesomeIcon icon={faCircleArrowRight} color="#fff" />
+      </div>
+    )
+  }
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FontAwesomeIcon icon={faCircleArrowLeft} color="#fff" />
+      </div>
+    )
+  }
+
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  }
+  return (
+    <div className="container projects-page">
+      <div className="slider">
+        <Slider {...settings}>
+          {images.map((img, i) => (
+            <div className={i === imageIndex ? 'slide activeSlide' : 'slide'}>
+              <img src={img} alt={'image'} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  )
 }
